2213db4c4a2df11ced1769cebd3cb44dabeafd46
tranuongkyoto <tranduongkyoto@gmail.com>
2022-08-07 17:03:17 +0700

    fix admin fear: user manage

--- src/main/webapp/app/modules/administration/user-management/user-manage.tsx
+++ src/main/webapp/app/modules/administration/user-management/user-manage.tsx
@@ -1,11 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './campaign.scss';
 // import { Table } from 'reactstrap';
-import { AppContext } from 'app/provider/appContext';
 import { getEllipsisTxt, timeStampToDateTime } from 'app/web3utils';
 import { useNotificationCustom } from 'app/web3utils/notification';
+import axios from 'axios';
 import { ethers } from 'ethers';
-import MoralisType from 'moralis';
 import { translate } from 'react-jhipster';
 import { useMoralis, useMoralisCloudFunction, useWeb3ExecuteFunction } from 'react-moralis';
 import { useParams } from 'react-router-dom';
@@ -21,19 +20,85 @@ interface transactionType {
   tokenSymbol: string;
 }
 
+interface User {
+  ethAddress: string;
+  username: string;
+  email: string;
+  isUpdateProfile: boolean;
+  isAdmin: boolean;
+  createAt: string;
+  updatedAt: string;
+  isCryptoCampaignBlacklist: boolean;
+  isNFTAuctionBlacklist: boolean;
+}
+
 export default function UserManage() {
   const { account, Moralis } = useMoralis();
   const { id } = useParams<{ id: string }>();
   const [balanceOf, setBalanceOf] = useState<number>();
   const [transaction, setTransaction] = useState<transactionType[]>();
-  const [user, setUser] = useState<MoralisType.User[]>();
+  const [userList, setUserList] = useState<User[]>([]);
   const { fetch, data } = useMoralisCloudFunction('getUsers');
-  const { userList, setUserList } = useContext(AppContext);
+  // const { userList, setUserList } = useContext(AppContext);
   const contractProcessor = useWeb3ExecuteFunction();
   const { handleNewNotification } = useNotificationCustom();
   const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-  useEffect(() => {}, []);
+  const [sub, setSub] = useState(true);
+  useEffect(() => {
+    const getBalanceOf = async () => {
+      const res = await axios.get(
+        'https://syiflvfo6dcs.usemoralis.com:2053/server/functions/getUsers?_ApplicationId=Cd8QyvZnJDKwubQYunl0xuqn9my8FR9fM3KZcVss'
+      );
+      if (res.status == 200 && res?.data?.result) {
+        const user: User[] = res.data.result
+          .filter(item => item?.username != 'coreservices')
+          .map(item => {
+            const obj = {
+              ethAddress: item?.ethAddress,
+              username: item?.username,
+              email: item?.email,
+              isUpdateProfile: item?.isUpdateProfile,
+              isAdmin: item?.isAdmin,
+              createAt: item?.createdAt,
+              updatedAt: item.updatedAt,
+              isCryptoCampaignBlacklist: false,
+              isNFTAuctionBlacklist: false,
+            };
+            return obj;
+          });
+        const campaignStore = new ethers.Contract(
+          '0xc7E8AF279f798e6632f91708946B0e78b451DA3C',
+          campaignabi.abi,
+          provider.getSigner(account)
+        );
+        Promise.allSettled(user.map(async item => await campaignStore.isBlackLister(item.ethAddress))).then(values => {
+          console.log(values);
+          var newUserList: User[] = user.map((item, index) => {
+            console.log(values[index]?.value);
+            return {
+              ...item,
+              isCryptoCampaignBlacklist: values[index]?.value ? values[index]?.value : false,
+            };
+          });
+          setUserList(newUserList);
+          const nftStore = new ethers.Contract('0xAa6190916A4f25039A50de315081f419e8A55FE2', abi.abi, provider.getSigner());
+          Promise.allSettled(user.map(async item => await nftStore.isBlackLister(item.ethAddress))).then(values => {
+            console.log(values);
+            const newUserList2: User[] = newUserList.map((item, index) => {
+              console.log(values[index]?.value);
+              return {
+                ...item,
+                isNFTAuctionBlacklist: values[index]?.value ? values[index]?.value : false,
+              };
+            });
+            setUserList(newUserList2);
+            setSub(!sub);
+          });
+        });
+      }
+    };
+    getBalanceOf();
+  }, [sub]);
 
   if (!data) {
     return (
@@ -53,29 +118,20 @@ export default function UserManage() {
       </div>
     );
   }
-  if (data && userList.length == 0) {
-    console.log(JSON.parse(JSON.stringify(data)));
-    setUserList(JSON.parse(JSON.stringify(data)));
-  }
+  // if (data && userList.length == 0) {
+  //   console.log(JSON.parse(JSON.stringify(data)));
+  //   setUserList(JSON.parse(JSON.stringify(data)));
+  // }
 
-  const addCampaign = async (add: string, id: string) => {
+  const addCampaign = async (add: string) => {
     try {
-      const campaignStore = new ethers.Contract('0x1fcf3d0D9A9C4a0f02519c094DE4d326dbafdE98', campaignabi.abi, provider.getSigner());
-      const transaction = await campaignStore.addWhiteLister(add);
+      const campaignStore = new ethers.Contract('0xc7E8AF279f798e6632f91708946B0e78b451DA3C', campaignabi.abi, provider.getSigner());
+      const transaction = await campaignStore.addBlackLister(add);
       handleNewNotification('success', 'Contract is pending, Please wait! ');
       const res = await transaction.wait();
       if (res?.status == 1) {
         handleNewNotification('success', `Contract is confirmed with ${res?.confirmations} confirmations. Thank for!`);
-        const User = Moralis.Object.extend('_User', { useMasterkey: true });
-        const query = new Moralis.Query(User);
-        //console.log(id);
-        console.log(add, id);
-        query.equalTo('objectId', id);
-        console.log(query);
-        const user = await query.first({ useMasterKey: true });
-        console.log(user);
-        user.set('isCryptoWhiteLister', true);
-        await user.save();
+        setSub(!sub);
       }
     } catch (error: any) {
       console.log(error);
@@ -89,21 +145,13 @@ export default function UserManage() {
   };
   const addNft = async (add: string) => {
     try {
-      const nftStore = new ethers.Contract('0x1fcf3d0D9A9C4a0f02519c094DE4d326dbafdE98', abi.abi, provider.getSigner());
-      const transaction = await nftStore.addWhiteLister(add);
+      const nftStore = new ethers.Contract('0xAa6190916A4f25039A50de315081f419e8A55FE2', abi.abi, provider.getSigner());
+      const transaction = await nftStore.addBlackLister(add);
       handleNewNotification('success', 'Contract is pending, Please wait! ');
       const res = await transaction.wait();
       if (res?.status == 1) {
         handleNewNotification('success', `Contract is confirmed with ${res?.confirmations} confirmations. Thank for!`);
-        // const User = Moralis.Object.extend('_User', { useMasterkey: true });
-        // const query = new Moralis.Query(User);
-        // console.log(add, id);
-        // query.equalTo('objectId', id);
-        // console.log(query);
-        // const user = await query.first({ useMasterKey: true });
-        // console.log(user);
-        // user.set('isNFTWhiteLister', true);
-        // await user.save();
+        setSub(!sub);
       }
     } catch (error: any) {
       console.log(error);
@@ -117,18 +165,13 @@ export default function UserManage() {
   };
   const removeCampaign = async (add: string) => {
     try {
-      const campaignStore = new ethers.Contract('0x1fcf3d0D9A9C4a0f02519c094DE4d326dbafdE98', campaignabi.abi, provider.getSigner());
-      const transaction = await campaignStore.removeWhiteLister(add);
+      const campaignStore = new ethers.Contract('0xc7E8AF279f798e6632f91708946B0e78b451DA3C', campaignabi.abi, provider.getSigner());
+      const transaction = await campaignStore.removeBlackLister(add);
       handleNewNotification('success', 'Contract is pending, Please wait! ');
       const res = await transaction.wait();
       if (res?.status == 1) {
         handleNewNotification('success', `Contract is confirmed with ${res?.confirmations} confirmations. Thank for!`);
-        const User = Moralis.Object.extend('_User');
-        const query = new Moralis.Query(User);
-        query.equalTo('ethAddress', add);
-        const user = await query.first();
-        user.set('isNFTWhiteLister', true);
-        await user.save();
+        setSub(!sub);
       }
     } catch (error: any) {
       console.log(error);
@@ -142,21 +185,13 @@ export default function UserManage() {
   };
   const removeNft = async (add: string) => {
     try {
-      const nftStore = new ethers.Contract('0x1fcf3d0D9A9C4a0f02519c094DE4d326dbafdE98', abi.abi, provider.getSigner());
-      const transaction = await nftStore.removeWhiteLister(add);
+      const nftStore = new ethers.Contract('0xAa6190916A4f25039A50de315081f419e8A55FE2', abi.abi, provider.getSigner());
+      const transaction = await nftStore.removeBlackLister(add);
       handleNewNotification('success', 'Contract is pending, Please wait! ');
       const res = await transaction.wait();
       if (res?.status == 1) {
         handleNewNotification('success', `Contract is confirmed with ${res?.confirmations} confirmations. Thank for!`);
-        // const User = Moralis.Object.extend('_User', { useMasterkey: true });
-        // const query = new Moralis.Query(User);
-        // console.log(add, id);
-        // query.equalTo('objectId', id);
-        // console.log(query);
-        // const user = await query.first({ useMasterKey: true });
-        // console.log(user);
-        // user.set('isNFTWhiteLister', true);
-        // await user.save();
+        setSub(!sub);
       }
     } catch (error: any) {
       console.log(error);
@@ -168,7 +203,9 @@ export default function UserManage() {
       );
     }
   };
-
+  if (!userList) {
+    return <></>;
+  }
   return (
     <>
       {userList.length == 0 ? (
@@ -188,55 +225,53 @@ export default function UserManage() {
               columnsConfig="0.75fr 1fr 1fr 0.75fr 0.5fr 1fr 1fr 1fr 1fr"
               data={
                 userList.length > 1
-                  ? userList
-                      .filter(item => item.username != 'coreservices')
-                      .map(item => [
-                        getEllipsisTxt(item.ethAddress) || '---',
-                        item.username.toString().length > 10 ? item.username.slice(0, 10) + '...' : item.username || '---',
-                        item.email || '---',
-                        item.isUpdateProfile.toString(),
-                        item.isAdmin.toString(),
-                        timeStampToDateTime(item.createdAt.toString()),
-                        timeStampToDateTime(item.updatedAt.toString()),
-                        !item.isCryptoWhiteLister ? (
-                          <Button
-                            color="green"
-                            id="test-button-status"
-                            onClick={() => addCampaign(item.ethAddress, item.objectId)}
-                            text="Add"
-                            theme="status"
-                            type="button"
-                          />
-                        ) : (
-                          <Button
-                            color="red"
-                            id="test-button-status"
-                            onClick={() => removeCampaign(item.ethAddress)}
-                            text="Remove"
-                            theme="status"
-                            type="button"
-                          />
-                        ),
-                        !item.isNFTWhiteLister ? (
-                          <Button
-                            color="green"
-                            id="test-button-status"
-                            onClick={() => addNft(item.ethAddress)}
-                            text="Add"
-                            theme="status"
-                            type="button"
-                          />
-                        ) : (
-                          <Button
-                            color="red"
-                            id="test-button-status"
-                            onClick={() => removeNft(item.ethAddress)}
-                            text="Remove"
-                            theme="status"
-                            type="button"
-                          />
-                        ),
-                      ])
+                  ? userList.map(item => [
+                      getEllipsisTxt(item.ethAddress) || '---',
+                      item.username.toString().length > 10 ? item.username.slice(0, 10) + '...' : item.username || '---',
+                      item.email || '---',
+                      item.isUpdateProfile.toString(),
+                      item.isAdmin.toString(),
+                      timeStampToDateTime(item.createAt.toString()),
+                      timeStampToDateTime(item.updatedAt.toString()),
+                      !item.isCryptoCampaignBlacklist ? (
+                        <Button
+                          color="green"
+                          id="test-button-status"
+                          onClick={() => addCampaign(item.ethAddress)}
+                          text="Add"
+                          theme="status"
+                          type="button"
+                        />
+                      ) : (
+                        <Button
+                          color="red"
+                          id="test-button-status"
+                          onClick={() => removeCampaign(item.ethAddress)}
+                          text="Remove"
+                          theme="status"
+                          type="button"
+                        />
+                      ),
+                      !item.isNFTAuctionBlacklist ? (
+                        <Button
+                          color="green"
+                          id="test-button-status"
+                          onClick={() => addNft(item.ethAddress)}
+                          text="Add"
+                          theme="status"
+                          type="button"
+                        />
+                      ) : (
+                        <Button
+                          color="red"
+                          id="test-button-status"
+                          onClick={() => removeNft(item.ethAddress)}
+                          text="Remove"
+                          theme="status"
+                          type="button"
+                        />
+                      ),
+                    ])
                   : []
               }
               header={[
